dc5c6c8ea052dd05e7fbc9e71d5b7f5c9f8869e0
FridaHer <96099416+FridaHer@users.noreply.github.com>
2022-05-21 01:47:06 -0500

    hola_mundo

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import saludo from "./saludo"
+const app = () => {
+  return (
+    <div className="app">
+      <saludo/>
+    </div>
+  );
+}
+export default app
---
+++ src/saludo.jsx
@@ -0,0 +1,4 @@
+const Saludo = () => {
+    return <h1> Hola, soy yo otra vez!!</h1>;
+};
+export default Saludo;
